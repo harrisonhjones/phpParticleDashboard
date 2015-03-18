2e74c6eff1bcf9d03017d88974a5545afce8fd22
Harrison Jones <harrisonhjones@gmail.com>
2015-03-17 22:05:20 -0700

    Added firmware upload modal

--- pages/index.php
+++ pages/index.php
@@ -42,7 +42,7 @@ if($spark->listDevices() == true)
       Connected?
     </th>
     <th>
-      View Device Details
+      Device Actions
     </th>
   </tr>
 
@@ -64,6 +64,7 @@ if($spark->listDevices() == true)
         </td>
         <td>
           <a href="?p=device&deviceID=<?php echo $device['id']; ?>"><button type="button" class="btn btn-default">View Device</button></a>
+          <button type="button" class="btn btn-default" data-toggle="modal" data-target="#firmwareModal" data-device-name="<?php echo $device['name']; ?>" data-device-id="<?php echo $device['id']; ?>">Upload Firmware</button>
         </td>
       </tr>
     
@@ -80,3 +81,52 @@ else
 ?>
   </div>
 
+<!-- Modal -->
+<<div class="modal fade" id="firmwareModal" tabindex="-1" role="dialog" aria-labelledby="firmwareModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="firmwareModal">Upload Firmware</h4>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="index.php">
+          <input type="hidden" name="p" id="p" value="firmwareUpload"/>
+          <div class="form-group">
+            <label for="device-id" class="control-label">Device ID:</label>
+            <input type="text" class="form-control" name="device-id" id="device-id">
+          </div>
+          <div class="form-group">
+            <label for="firmwareFile">File input</label>
+            <input type="file" id="firmwareFile" name="firmwareFile" >
+            <p class="help-block">Please only select a firmware .bin file only.</p>
+          </div>
+        
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-default" data-dismiss="modal" type="reset">Close</button>
+        <button class="btn btn-primary" type="submit">Upload Firmware</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php
+$endPageContent = "
+<script>
+$('#firmwareModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var deviceID = button.data('device-id') // Extract info from data-* attributes
+  var deviceName = button.data('device-name') // Extract info from data-* attributes
+
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('Upload firmware to \'' + deviceName + '\'')
+  modal.find('#device-id').val(deviceID)
+})
+</script>";
+?>
