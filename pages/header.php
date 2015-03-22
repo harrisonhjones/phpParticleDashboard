d5a1721ddbfe4a9615ab146da52fb33f00708811
Harrison Jones <harrisonhjones@gmail.com>
2015-03-22 11:46:10 -0700

    Added new favicon icons/pngs

--- pages/header.php
+++ pages/header.php
@@ -10,12 +10,13 @@ error_reporting(-1);
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <link rel="icon" href="favicon.png" />
+        <link rel="shortcut icon" href="favicon.png" />
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" href="favicon.ico">
 
         <title>phpSpark Example Dashboard</title>
 
@@ -35,7 +36,6 @@ error_reporting(-1);
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-
         <style>
             body {
                 padding-bottom: 2em;
